f7a449ed30d80f835b6e3952f5495e62e1abef78
JP Candelier <jp.candelier@zefr.com>
2016-03-15 19:44:48 -0700

    babel and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel',
+              query: {
+                presets: ['react', 'es2015']
+              }
+            }
+        ]
     }
 }
